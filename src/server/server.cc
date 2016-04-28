35cffa2301a06422d0f1a60b4457236822a51fab
Victor Jimenez <betabandido@gmail.com>
2016-04-28 15:16:48 +0000

    Added initial server code.

---
+++ src/server/server.cc
@@ -0,0 +1,99 @@
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+#include <boost/asio.hpp>
+#include <boost/format.hpp>
+
+#include <portaudiocpp/PortAudioCpp.hxx>
+
+using boost::asio::ip::tcp;
+namespace asio = boost::asio;
+namespace pa = portaudio;
+
+static constexpr unsigned short PORT = 19999;
+
+static constexpr double SAMPLE_RATE = 44100.0;
+static constexpr unsigned NUM_CHANNELS = 2;
+static constexpr bool INTERLEAVED_STREAM = true;
+// frame size = 2 channels x 32-bit per sample = 8 bytes
+static constexpr unsigned FRAME_SIZE = 4 * NUM_CHANNELS;
+
+static constexpr size_t BUFFER_SIZE = 4096;
+
+static std::unique_ptr<pa::BlockingStream> create_audio_stream() {
+  auto& system = pa::System::instance();
+
+  pa::DirectionSpecificStreamParameters out_params(
+      system.defaultOutputDevice(),
+      NUM_CHANNELS,
+      pa::FLOAT32,
+      INTERLEAVED_STREAM,
+      system.defaultOutputDevice().defaultHighOutputLatency(),
+      nullptr);
+
+  pa::StreamParameters params(
+      pa::DirectionSpecificStreamParameters::null(),
+      out_params,
+      SAMPLE_RATE,
+      paFramesPerBufferUnspecified,
+      paClipOff);
+
+  return std::make_unique<pa::BlockingStream>(params);
+}
+
+void session(tcp::socket socket) {
+  try {
+    std::cout << "Session started\n";
+    auto stream = create_audio_stream();
+
+    stream->start();
+
+    while (true) {
+      std::array<char, BUFFER_SIZE> buffer;
+      boost::system::error_code ec;
+      auto length = asio::read(
+          socket,
+          asio::buffer(buffer, BUFFER_SIZE), 
+          asio::transfer_at_least(BUFFER_SIZE),
+          ec);
+      // TODO what if length > 0 && eof ??
+      if (ec == asio::error::eof)
+        break; // connection closed cleanly by peer
+      else if (ec)
+        throw boost::system::system_error(ec);
+
+      std::cout << boost::format("%1% bytes received\n") % length;
+      stream->write(buffer.data(), length / FRAME_SIZE);
+    }
+
+    stream->stop();
+    stream->close();
+  } catch (const std::exception& e) {
+    std::cerr << boost::format("Exception in thread: %1%\n") % e.what();
+  }
+}
+
+void server(asio::io_service& io_service, unsigned short port) {
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
+  std::cout << "Server started\n";
+  while (true) {
+    tcp::socket socket(io_service);
+    acceptor.accept(socket);
+    std::thread(session, std::move(socket)).detach();
+  }
+}
+
+int main() {
+  try {
+    pa::AutoSystem auto_system;
+    asio::io_service io_service;
+    server(io_service, PORT);
+  } catch (const std::exception& e) {
+    std::cerr << boost::format("Exception: %1%\n") % e.what();
+  }
+
+  return 0;
+}
+
